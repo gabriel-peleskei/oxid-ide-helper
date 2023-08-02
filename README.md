fcba134f2f1cfe36619a385c178179aa76359aca
Gabriel Peleskei <gabriel.peleskei@gmail.com>
2023-08-02 21:13:24 +0000

    Runnable version

---
+++ README.md
@@ -0,0 +1,13 @@
+# IDE HELPER
+
+## Installation
+
+Using composer:
+```bash
+composer require gp/oxid-ide-helper
+```
+
+## Usage
+```bash
+vendor/bin/oe-console gp:ide:helper -h
+```
--- src/Command/IdeHelper.php
+++ src/Command/IdeHelper.php
@@ -1,41 +1,36 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace GP\Oxid\IdeHelper\Command;
-use OxidEsales\EshopCommunity\Internal\Framework\Database\QueryBuilderFactoryInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ModuleConfigurationDaoInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\ShopConfigurationDaoInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Facade\ActiveModulesDataProviderInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Install\Service\ModuleConfigurationInstallerInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\Setup\Service\ModuleActivationServiceInterface;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\State\ModuleStateService;
-use OxidEsales\EshopCommunity\Internal\Framework\Module\State\ModuleStateServiceInterface;
-use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
+
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Dao\Chain\ClassExtensionsChainDaoInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Path;
 
 class IdeHelper extends Command {
-    protected $shopIds;
+    protected ?int $shopId = null;
     protected InputInterface $input;
     protected OutputInterface $output;
 
     public function __construct(
-        protected ShopConfigurationDaoInterface $shopConfigurationDao,
-        protected ContextInterface $context,
-        protected ModuleStateServiceInterface $moduleStateService,
-        protected ModuleActivationServiceInterface $moduleActivationService,
-        protected ModuleConfigurationInstallerInterface $moduleConfigurationInstaller,
-        protected ModuleConfigurationDaoInterface $moduleConfigurationDao,
-        protected QueryBuilderFactoryInterface $queryBuilderFactory,
-        protected ActiveModulesDataProviderInterface $modProvider
+        protected ClassExtensionsChainDaoInterface $chainProvider
     ) {
         parent::__construct(null);
     }
     protected function configure() {
-        $this->setName('gp:ide-helper');
+        $this->setName('gp:ide:helper');
+        // $this->addOption('');
         $this->setDescription('Create IDE-Helper file');
         $help = <<<EOT
-    Apply module activation/deactivation by given state through configuration.
+<info>Create <fg=gray;options=bold>.ide-helper.php</> File withing <fg=gray;options=bold>/source/modules</> directory for the given shop.</info>
+If you have a multishop system, use <comment>--shop-id=2</comment> to define the shop's module chain.
+
+<comment>Notice:</comment>
+Only <fg=gray>installed</> and <fg=gray>known</> class extensions are considered. 
+Basically, it is the representation of the <fg=gray>var/configuration/shops/<SHOP_ID>/class_extension_chain.yaml</> file.
+
+
 EOT;
 
         $this->setHelp($help);
@@ -45,18 +40,57 @@ EOT;
     protected function execute(InputInterface $input, OutputInterface $output) {
         $this->input = $input;
         $this->output = $output;
-        $shopIds = $this->getSelectedShopIds();
-        $mods = $this->modProvider->getClassExtensions();
-        $output->writeln(print_r($mods, true));
+        $shopId = $this->getShopId();
+        $chain = $this->getChain($shopId);
+        $extensions = [];
+        foreach ($chain as $parent => $items) {
+            // track last class of chain
+            $last = null;
+            foreach ($items as $new) {
+                $extensions[] = $this->getExtension($last ?? $parent, $new);
+                $last = $new;
+            }
+        }
+        $filePath = $this->getModulesDir('.ide-helper.php');
+        $output->writeln("Writing helper file for <comment>shop-id</comment>=<comment>{$shopId}</comment> to <comment>{$filePath}</comment>");
+        if ($this->makeFile($extensions, $filePath)) {
+            $output->writeln("<info>File has been created</info>");
+        } else {
+            $output->writeln('<error>File could not be written</error>');
+        }
         return 0;
     }
 
 
-    protected function getSelectedShopIds() {
+    protected function getShopId(): int {
         try {
-            return $this->shopIds ??= [ (int) $this->input->getOption('shop-id')];
+            return $this->shopId ??= (int) $this->input->getOption('shop-id');
         } catch (\InvalidArgumentException $e) {
-            return $this->shopIds ??= $this->context->getAllShopIds();
+            return $this->shopId ??= 1;
         } 
+    }
+
+    protected function getChain(int $shopId): array {
+        return $this->chainProvider->getChain($shopId)->getChain();
+    }
+
+    protected function getModulesDir(string ... $path): string {
+        return Path::join(OX_BASE_PATH, 'modules', ...$path);
+    }
+
+    protected function getExtension(string $parent, string $new): string {
+        $namespaceList = explode('\\', $new);   
+        $className = array_pop($namespaceList);
+        $namespace = implode('\\', $namespaceList);
+        return "namespace $namespace { class {$className}_parent extends \\$parent {} }";
+    }
+
+    protected function makeFile(array $paths, string $filePath): bool {
+        $contents = implode("\n", $paths);
+        $result = file_put_contents($filePath, "<?php // IDE Helper for shop {$this->shopId}\n{$contents}");
+        if (false === $result) {
+            return false;
+        }
+        return true;
     }
 }
